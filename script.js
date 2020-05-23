1c6ddd31121d470a09712515a7da0651c7cb423b
PAULO_SILVA <paulo.venones@gmail.com>
2020-05-23 19:12:03 -0300

    Front-end do Foodfy completo

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const recipes = document.querySelectorAll(".recipe")
 modalOverlay = document.querySelector(".modal-overlay")
+modalContent = document.querySelector(".modal-content")
+
+for(let recipe of recipes) {
+    recipe.addEventListener("click", function() {
+        modalOverlay.classList.add("active")
+        const img = recipe.querySelector(".recipe-image")
+        const imgUrl = img.getAttribute('src')
+
+        const title = recipe.querySelector(".recipe-title").textContent
+        const by = recipe.querySelector(".by").textContent
+
+        modalContent.querySelector("img").src = `${imgUrl}`
+        modalContent.querySelector(".modal-title").textContent = `${title}`
+        modalContent.querySelector(".by-modal").textContent = `${by}`
+    })
+}
 
 document.querySelector('.close-modal').addEventListener("click", function() {
     modalOverlay.classList.remove('active')
 })
-
-for(let recipe of recipes) {
-    addEventListener("click", function() {
-        modalOverlay.classList.add("active")
-    })
-}
